adb8a62ad91b505fc28ab0c39b367f67f81928cf
nicolasgavriil <219146079+nicolasgavriil@users.noreply.github.com>
2026-03-18 19:53:51 +0100

    Complete phonebook 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 
 const Filter = ({searchName, onChange}) => {
   return(
@@ -39,12 +41,22 @@ const Person = ({person}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
+
+  useEffect(() => {
+    const fetchPersons = async() => {
+      console.log("Fetch persons from json server");
+      try {
+        const response = await axios.get('http://localhost:3001/persons');
+        setPersons(response.data);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    
+    fetchPersons();
+  }, [])
+
   const blankPerson = {
     name: '',
     number: '',
