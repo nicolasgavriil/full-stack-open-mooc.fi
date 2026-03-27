0513e6f376bf41bdaa5ff04490e8500f04b5d889
nicolasgavriil <219146079+nicolasgavriil@users.noreply.github.com>
2026-03-27 23:53:48 +0100

    Complete bloglist 4.8

--- part4/bloglist/backend/controllers/blogs.js
+++ part4/bloglist/backend/controllers/blogs.js
@@ -4,22 +4,14 @@ import { Blog } from "../models/blog.js";
 const blogsRouter = Router();
 
 blogsRouter.get("/", async (req, res, next) => {
-  try {
-    const blogs = await Blog.find({});
-    return res.status(200).json(blogs);
-  } catch (err) {
-    next(err);
-  }
+  const blogs = await Blog.find({});
+  return res.status(200).json(blogs);
 });
 
 blogsRouter.post("/", async (req, res, next) => {
-  try {
-    const blog = new Blog(req.body);
-    const result = await blog.save();
-    return res.status(201).json(result);
-  } catch (err) {
-    next(err);
-  }
+  const blog = new Blog(req.body);
+  const result = await blog.save();
+  return res.status(201).json(result);
 });
 
 export default blogsRouter;
