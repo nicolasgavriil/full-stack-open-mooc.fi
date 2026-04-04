45c3335dc68123617ca6ddba8b5c2e13cecde1b9
nicolasgavriil <219146079+nicolasgavriil@users.noreply.github.com>
2026-04-04 04:05:04 +0000

    Complete bloglist 5.9 and 5.10

--- part5/bloglist/frontend/components/BlogList.jsx
+++ part5/bloglist/frontend/components/BlogList.jsx
@@ -1,9 +1,10 @@
 import Blog from "./Blog.jsx";
 
 const BlogList = ({ blogs, onLikeBlog }) => {
+  const sortedBlogs = blogs.toSorted((a, b) => b.likes - a.likes);
   return (
     <>
-      {blogs.map((blog) => (
+      {sortedBlogs.map((blog) => (
         <Blog key={blog.id} blog={blog} onLikeBlog={onLikeBlog} />
       ))}
     </>
