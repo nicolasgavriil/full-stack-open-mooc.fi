71ab7dab769547846d5534a83230f23e8a1cf4b7
nicolasgavriil <219146079+nicolasgavriil@users.noreply.github.com>
2026-04-04 01:35:04 +0000

    Complete bloglist 5.7

--- part5/bloglist/frontend/components/Blog.jsx
+++ part5/bloglist/frontend/components/Blog.jsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
+
 const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const toggleDetails = () => {
+    setShowDetails((prev) => !prev);
+  };
+
   return (
-    <div>
-      {blog.title} {blog.author}
+    <div className="blog">
+      {blog.title} {blog.author}{" "}
+      <button type="button" onClick={toggleDetails}>
+        {showDetails ? "hide" : "view"}
+      </button>
+      {showDetails && (
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes} <button type="button">like</button>
+          </div>
+          <div>{blog.user.name}</div>
+        </div>
+      )}
     </div>
   );
 };
