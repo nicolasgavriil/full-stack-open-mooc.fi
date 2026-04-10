bae8398223a1d0db2d1ce479fc4b78485ef02fa8
nicolasgavriil <219146079+nicolasgavriil@users.noreply.github.com>
2026-04-10 10:22:19 +0000

    Complete anecdotes-zustand 6.7

--- part6/anecdotes-zustand/src/App.jsx
+++ part6/anecdotes-zustand/src/App.jsx
@@ -1,8 +1,16 @@
+import { useEffect } from "react";
+import { useAnecdoteActions } from "./store.js";
 import AnecdoteList from "./components/AnecdoteList.jsx";
 import AnecdoteForm from "./components/AnecdoteForm.jsx";
 import Filter from "./components/Filter.jsx";
 
 const App = () => {
+  const { initialize } = useAnecdoteActions();
+
+  useEffect(() => {
+    initialize();
+  }, [initialize]);
+
   return (
     <div>
       <Filter />
